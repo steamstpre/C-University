74a879364721d5b57db30fef880f2359ef3d1168
Danylo <danylo.minko@globallogic.com>
2022-05-26 17:07:48 +0300

    task_4

---
+++ WebApplication1 2/WebApplication1/Controllers/ReceiptController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiptController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ReceiptController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                select Id as ""Id"",
+                        companyName as ""companyName""
+                        vatRate as ""vatRate""
+                        dateOfPurcase as ""dateOfPurcase""
+                        vatCode as ""vatCode""
+                        country as ""country""
+                        dateOfTaxRegistration as ""dateOfTaxRegistration""
+                from Department
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ReceiptAppCon");
+            NpgsqlDataReader myReader;
+            using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+    }
+}
---
+++ WebApplication1 2/WebApplication1/Models/Receipt.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+
+
+namespace WebApplication1.Models
+{
+    public class TaxFree
+    {
+        private string id;
+        private string companyName;
+        private int vatRate;
+        private DateTime dateOfPurcase;
+        private string vatCode;
+        private String country;
+        private DateTime dateOfTaxRegistration;
+
+        public TaxFree InputData()
+        {
+            foreach (PropertyDescriptor prop in TypeDescriptor.GetProperties(this))
+            {
+                Console.Write($"{prop.Name}: ");
+                prop.SetValue(this, Convert.ChangeType(Console.ReadLine(), prop.PropertyType));
+            }
+
+            return this;
+        }
+
+        public string Id { get; set; }
+
+        public string Company
+        {
+            get => companyName;
+            set => companyName = Validation.setComapnyName(value);
+        }
+
+        public int VatRate
+        {
+            get => vatRate;
+            set => vatRate = Validation.setVatRate(value);
+        }
+
+        public DateTime DateOfPurchase
+        {
+            get => dateOfPurcase;
+            set => dateOfPurcase = Validation.setDateOfPurchase(value);
+        }
+
+        public string VatCode
+        {
+            get => vatCode;
+            set => vatCode = Validation.setVatCode(value);
+        }
+
+        public string Country
+        {
+            get => country;
+            set => country = Validation.setCountry(value);
+        }
+
+        public DateTime DateOfTaxRegistration
+        {
+            get => dateOfTaxRegistration;
+            set => dateOfTaxRegistration = Validation.setDataOfRefistrationTax(value, dateOfPurcase);
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+            foreach (PropertyDescriptor prop in TypeDescriptor.GetProperties(this))
+                res += ($"{prop.Name}: {prop.GetValue(this)}\n");
+            return res.Substring(0, res.Length - 1);
+        }
+
+
+    }
+}
